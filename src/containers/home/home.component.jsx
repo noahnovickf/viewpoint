4d43db648ccdbd5aa3d4636cec2fbb6a27817a05
nnovickf <nnovickfaille@gmail.com>
2020-05-08 13:45:38 -0400

    trying to read user signed in info

--- src/containers/home/home.component.jsx
+++ src/containers/home/home.component.jsx
@@ -12,12 +12,33 @@ const Home = (props) => {
 
   //google sign in code
   var provider = new firebase.auth.GoogleAuthProvider();
+
   const googleSignUp = () => {
+    console.log("logging in...");
+    firebase.auth().signInWithRedirect(provider);
+    //should be what goes after ^
     firebase
       .auth()
-      .signInWithRedirect(provider)
-      .then((res) => {
-        console.log(res);
+      .getRedirectResult()
+      .then(function (result) {
+        if (result.credential) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user);
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
       });
   };
 
