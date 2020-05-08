1348acc43c3c2d3bca1edd8bb0da948da93c88a7
nnovickf <nnovickfaille@gmail.com>
2020-05-08 17:33:26 -0400

    logs in user and displays current user obj

--- src/containers/home/home.component.jsx
+++ src/containers/home/home.component.jsx
@@ -8,54 +8,54 @@ const Home = (props) => {
   const { fetchUserThunk } = props;
 
   // This is how to fetch data from the global state
-  const user = useSelector((state) => state.users.user);
+  const users = useSelector((state) => state.users.user);
 
   //google sign in code
   var provider = new firebase.auth.GoogleAuthProvider();
 
   const googleSignUp = () => {
     console.log("logging in...");
-    firebase.auth().signInWithRedirect(provider);
-    //should be what goes after ^
     firebase
       .auth()
-      .getRedirectResult()
-      .then(function (result) {
-        if (result.credential) {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          var token = result.credential.accessToken;
-          // ...
-        }
-        // The signed-in user info.
-        var user = result.user;
-        console.log(user);
-      })
-      .catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
+      .signInWithPopup(provider)
+      .then(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+          console.log(user);
+        });
       });
   };
 
-  return (
-    <div className="w-full max-w-md">
-      <div className="flex justify-center">{JSON.stringify(user)}</div>
-      <button className="bg-blue w-full bg-orange-600" onClick={fetchUserThunk}>
-        FETCH A USER
-      </button>
+  const getUserInfo = () => {
+    console.log("pressed");
+    console.log(firebase.auth().currentUser);
+  };
 
-      {/* Use of presentational components
-       * Simply pass down props and display UI
-       */}
-      <SampleDummy text="Sample Text" />
-      <button onClick={googleSignUp}>Sign up with google</button>
-    </div>
-  );
+  if (true) {
+    return (
+      <div className="w-full max-w-md">
+        <div className="flex justify-center">{JSON.stringify(users)}</div>
+        <button
+          className="bg-blue w-full bg-orange-600"
+          onClick={fetchUserThunk}
+        >
+          FETCH A USER
+        </button>
+
+        {/* Use of presentational components
+         * Simply pass down props and display UI
+         */}
+        <SampleDummy text="Sample Text" />
+        <div className="m-10">
+          <button onClick={googleSignUp}>Sign up with google</button>
+        </div>
+        <div className="m-10">
+          <button onClick={getUserInfo}>get Current user</button>
+        </div>
+      </div>
+    );
+  } else {
+    return <h2>HI</h2>;
+  }
 };
 
 export default Home;
