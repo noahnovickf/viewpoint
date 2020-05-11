0a41563cfb35c127bce4fb7b3d65ca423de97d22
nnovickf <nnovickfaille@gmail.com>
2020-05-11 12:18:40 -0400

    login logic in app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Home from "./containers/home";
 import Login from "./containers/login";
+import { useSelector } from "react-redux";
 
 function App() {
-  return (
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    setUser(userFromState);
+  }, []);
+
+  const userFromState = useSelector((state) => state.users.user);
+
+  return !userFromState.full_name ? (
     <div className="flex justify-center items-center">
       <Login />
+    </div>
+  ) : (
+    <div className="flex justify-center items-center">
+      <Home />
     </div>
   );
 }
--- src/containers/login/login.component.jsx
+++ src/containers/login/login.component.jsx
@@ -1,19 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { auth } from "database";
-import { useSelector } from "react-redux";
-import Home from "containers/home";
+import React from "react";
 
 const Login = (props) => {
-  const [user, setUser] = useState({});
-
-  useEffect(() => {
-    setUser(userFromState);
-  }, []);
-
-  const userFromState = useSelector((state) => state.users.user);
-  console.log("state", user);
-  console.log(userFromState.full_name);
-  return !userFromState.full_name ? (
+  return (
     <div className="flex flex-col justify-center w-full max-w-md">
       <div className="flex justify-center">Please sign in with google</div>
       <button
@@ -23,8 +11,6 @@ const Login = (props) => {
         Sign in With Google
       </button>
     </div>
-  ) : (
-    <Home />
   );
 };
 export default Login;
