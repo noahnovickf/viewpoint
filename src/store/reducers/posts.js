6dbb110c5db8b655899fcb0b1e5d0c8e18428744
Rahul <37151383+lolcode8@users.noreply.github.com>
2020-05-15 21:36:36 -0400

    Update src/store/reducers/posts.js

--- src/store/reducers/posts.js
+++ src/store/reducers/posts.js
@@ -4,7 +4,7 @@ const INITIAL_POST_STATE = {
 
 export default (state = INITIAL_POST_STATE, action) => {
   switch (action.type) {
-    case "POSTS_FETHCED":
+    case "POSTS_FETCHED":
       return {
         ...state,
         posts: action.payload,
