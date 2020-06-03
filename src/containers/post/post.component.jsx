9b8bdbed85120bbfeac0f31e3c649742cee6008a
nnovickf <nnovickfaille@gmail.com>
2020-06-03 12:30:16 -0400

    change view of vote to appear after vote

--- src/containers/post/post.component.jsx
+++ src/containers/post/post.component.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { voteForOption } from "database/votePost";
 
 const Post = (props) => {
-  console.log(props);
+  const [voteView, setVoteView] = useState(false);
+  //console.log(props);
+
+  const handleVote = (obj) => {
+    setVoteView(true);
+    voteForOption(obj);
+  };
   return (
     <li className="rounded border-4 border-orange-600 mt-2">
       <h6>{props.body}</h6>
@@ -11,7 +17,7 @@ const Post = (props) => {
         <button
           className="bg-gray-300 hover:bg-gray-400 w-20 h-10 rounded-l-lg border-r-2 border-gray-600"
           onClick={() =>
-            voteForOption({ optionName: "option_a", postId: props.id })
+            handleVote({ optionName: "option_a", postId: props.id })
           }
         >
           {props.optionAName}
@@ -19,18 +25,26 @@ const Post = (props) => {
         <button
           className="bg-gray-300 hover:bg-gray-400 w-20 h-10 rounded-r-lg "
           onClick={() =>
-            voteForOption({ optionName: "option_b", postId: props.id })
+            handleVote({ optionName: "option_b", postId: props.id })
           }
         >
           {props.optionBName}
         </button>
       </div>
       <div className="flex justify-center">
-        <div className="bg-gray-300 hover:bg-gray-400 w-20 h-10 rounded-l-lg border-r-2 border-gray-600">
-          {props.optionAName}: {props.optionA}
+        <div
+          className={`${
+            voteView ? "show" : "hidden"
+          } bg-gray-300 hover:bg-gray-400 w-20 h-10 rounded-l-lg border-r-2 border-gray-600`}
+        >
+          {props.optionAName}: {props.optionA + 1}
         </div>
-        <div className="bg-gray-300 hover:bg-gray-400 w-20 h-10 rounded-r-lg">
-          {props.optionBName}: {props.optionB}
+        <div
+          className={`${
+            voteView ? "show" : "hidden"
+          } bg-gray-300 hover:bg-gray-400 w-20 h-10 rounded-r-lg`}
+        >
+          {props.optionBName}: {props.optionB + 1}
         </div>
       </div>
     </li>
