0f354132a88310a81ec2b1634a7b27ab8fea3791
nnovickf <nnovickfaille@gmail.com>
2020-06-03 13:28:50 -0400

    remove vote options after vote

--- src/containers/post/post.component.jsx
+++ src/containers/post/post.component.jsx
@@ -24,7 +24,9 @@ const Post = (props) => {
       {/* <p>{props.created_at}</p> */}
       <div className="flex justify-center">
         <button
-          className="bg-gray-300 hover:bg-gray-400 w-20 h-10 rounded-l-lg border-r-2 border-gray-600"
+          className={`${
+            voteView ? "hidden" : "show"
+          } bg-gray-300 hover:bg-gray-400 w-20 h-10 rounded-l-lg border-r-2 border-gray-600`}
           onClick={() =>
             handleVote({ optionName: "option_a", postId: props.id })
           }
@@ -32,7 +34,9 @@ const Post = (props) => {
           {props.optionAName}
         </button>
         <button
-          className="bg-gray-300 hover:bg-gray-400 w-20 h-10 rounded-r-lg "
+          className={`${
+            voteView ? "hidden" : "show"
+          } bg-gray-300 hover:bg-gray-400 w-20 h-10 rounded-r-lg `}
           onClick={() =>
             handleVote({ optionName: "option_b", postId: props.id })
           }
