1c9413196fe7c8c2c0299c1a1443894bb0dd28ac
nnovickf <nnovickfaille@gmail.com>
2020-06-10 11:25:49 -0400

    login page styled

--- src/containers/login/login.component.jsx
+++ src/containers/login/login.component.jsx
@@ -8,15 +8,19 @@ const Login = (props) => {
   if (isUserLoggedIn) props.history.push("/");
 
   return (
-    <div className="bg-bluey">
-      <h1 className="text-center text-grayy text-xl">Welcome to D-Side</h1>
-      <div className="flex justify-center">
-        <button onClick={props.signInWithGoogleThunk}>
-          <img
-            src="https://developers.google.com/identity/images/btn_google_signin_light_normal_web.png"
-            alt="Google"
-          ></img>
-        </button>
+    <div class="bg-blueGray flex flex-col items-center justify-center text-center h-screen  ">
+      <div className="">
+        <h1 className=" typewriter text-grayy text-center text-xl font-mono">
+          Welcome to Undefined.
+        </h1>
+        <div className="object-center pt-8">
+          <button onClick={props.signInWithGoogleThunk}>
+            <img
+              src="https://developers.google.com/identity/images/btn_google_signin_light_normal_web.png"
+              alt="Google"
+            ></img>
+          </button>
+        </div>
       </div>
     </div>
   );
