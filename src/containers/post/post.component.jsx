3bc0a979acfdd11cd49b3f7150ee84be5a89734d
nnovickf <nnovickfaille@gmail.com>
2020-06-25 16:21:24 -0400

    show which vote you choose

--- src/containers/post/post.component.jsx
+++ src/containers/post/post.component.jsx
@@ -116,6 +116,8 @@ const Post = (props) => {
             voteAPercent === 100
               ? " rounded-lg mr-2 "
               : " rounded-l-lg border-r-2 border-gray-600 "
+          } ${
+            hasUserVotedForB ? "opacity-50" : " "
           } flex items-center text-center justify-center align-middle bg-pinky h-10 ml-2 mb-2`}
           style={{ width: voteAPercent + "%" }}
         >
@@ -127,9 +129,9 @@ const Post = (props) => {
         <div
           className={`${
             canUserViewVote && voteAPercent !== 100 ? "show" : "hidden"
-          } ${
-            voteBPercent === 100 ? " rounded-lg ml-2 " : " rounded-r-lg "
-          } flex items-center justify-center text-center  bg-orangy h-10 mr-2 mb-2`}
+          } ${voteBPercent === 100 ? " rounded-lg ml-2 " : " rounded-r-lg "} ${
+            hasUserVotedForA ? "opacity-50" : " "
+          } items-center justify-center text-center  bg-orangy h-10 mr-2 mb-2`}
           style={{ width: voteBPercent + "%" }}
         >
           <div>
